2278a674097823b09604d74b8f8ae672b8099194
Raldienpr325 <aldinprayoga26@webmail.umm.ac.id>
2022-03-05 13:56:24 +0700

    1st

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('12345678')
+        ]);
+    
+        User::create([
+            'name' => 'user',
+            'email' => 'user@example.com',
+            'password' => bcrypt('12345678')
+        ]);
+    }
+}
